ad84fa7b97e012f6ccc4eaa26453e4e21008efba
Saulius Svirmickas <dozess@gmail.com>
2025-10-25 20:05:29 +0000

    Try fixing this error: `Build Error: Error: Failed to read source code f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { FirebaseClientProvider } from '@/firebase/client-provider';
-import { dir } from 'i18next'
 import { PT_Sans } from 'next/font/google';
-import { fallbackLng } from './i18n/settings';
 
 const ptSans = PT_Sans({
   subsets: ['latin'],
@@ -23,9 +21,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const lng = fallbackLng;
   return (
-    <html lang={lng} dir={dir(lng)} suppressHydrationWarning>
+    <html lang="lt" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
